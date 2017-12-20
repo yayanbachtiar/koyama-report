aca678e316f4bd671ff92e14d093b13d0623f8b2
subchan yayan bachtiar <yayan.bachtiar@ralali.com>
2017-12-20 09:41:56 +0700

    add default date

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,6 @@
-$('input.date').datepicker({format: 'yyyy-mm-dd'});
+$('input.date').datepicker({
+    format: 'yyyy-mm-dd'
+})
+var myDate = new Date();
+  var date = myDate.getFullYear() + '-' + ('0'+ myDate.getMonth()+1).slice(-2) + '-' + ('0'+ myDate.getDate()).slice(-2);
+  $("input.date").val(date);
